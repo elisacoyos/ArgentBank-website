1e54fe4098c557c6ff292c287654e94c212ea2fa
elisacoyos <elisacoyos@gmail.com>
2024-02-22 17:47:05 +0100

    LoginPage-ComponentForm

--- Frontend/src/pages/LoginPage/LoginPage.jsx
+++ Frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,16 @@
-const LoginPage=()=>{
-    return(
-        <div>Login Page</div>
-    )
-}
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import Form from '../../components/Form/Form';
+
+const LoginPage = () => {
+  return (
+    <div className='page_login'>
+      <Navbar />
+      <Form />
+      <Footer />
+    </div>
+  );
+};
 
 export default LoginPage;
